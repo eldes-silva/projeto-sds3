aec414062b00eba9b9791aa26b997192603ca240
eldes-silva <eldesflash@hotmail.com>
2021-05-08 23:44:30 -0300

    Basic Statics Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBbar";
+
 function App() {
   return (
-    <div className="App">
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá Mundo!</h1>
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
